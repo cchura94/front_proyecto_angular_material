dce0f343ee3fff1a026d6c7addfface09d3dcdaa
cristian <cchura.cpc@gmail.com>
2024-05-13 21:14:42 -0400

    registrar modulos y componentes + rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadChildren: () => import("./web/web.module").then(w => w.WebModule) },
+    { path: 'auth', loadChildren: () => import("./auth/auth.module").then(a => a.AuthModule) },
+    { path: 'admin', loadChildren: () => import("./admin/admin.module").then(ad => ad.AdminModule) },
+];
